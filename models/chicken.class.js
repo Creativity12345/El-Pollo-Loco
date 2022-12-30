dd66a2b29c9dab479d599b5ba1988a1932843279
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2022-12-30 12:52:31 +0100

    add air background

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,5 @@
 class Chicken extends MovableObject{
-    y = 340;
+    y = 330;
 
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,6 +1,6 @@
 class MovableObject {
     x = 120;
-    y = 330;
+    y = 325;
     img;
     height = 100;
     width = 100;
