e072f865e0773ca2135e724ab0adf8ddfdcbffc4
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-01-09 16:30:49 +0100

    enter fullscreen functions

--- js/game.js
+++ js/game.js
@@ -4,11 +4,11 @@ let keyboard = new Keyboard();
 
 
 // function startScreen(){
-//     document.getElementById('canvas').innerHTML = '<img src="img/9_intro_outro_screens/start/startscreen_1.png" style="width: 100%; height: 100%;">';
+//     document.getElementById('test').innerHTML = '<img src="img/9_intro_outro_screens/start/startscreen_1.png" style="width: 100%; height: 100%;">';
 // }
 
 
-function init(){
+function init() {
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
 }
@@ -20,23 +20,23 @@ window.addEventListener("keydown", (e) => {
     }
 
     if (e.keyCode == 37) {
-        keyboard.LEFT = true;        
+        keyboard.LEFT = true;
     }
 
     if (e.keyCode == 38) {
-        keyboard.UP = true;        
+        keyboard.UP = true;
     }
 
     if (e.keyCode == 40) {
-        keyboard.DOWN = true;        
+        keyboard.DOWN = true;
     }
 
     if (e.keyCode == 32) {
-        keyboard.SPACE = true;        
+        keyboard.SPACE = true;
     }
 
     if (e.keyCode == 68) {
-        keyboard.D = true;        
+        keyboard.D = true;
     }
 });
 
@@ -47,22 +47,54 @@ window.addEventListener("keyup", (e) => {
     }
 
     if (e.keyCode == 37) {
-        keyboard.LEFT = false;        
+        keyboard.LEFT = false;
     }
 
     if (e.keyCode == 38) {
-        keyboard.UP = false;        
+        keyboard.UP = false;
     }
 
     if (e.keyCode == 40) {
-        keyboard.DOWN = false;        
+        keyboard.DOWN = false;
     }
 
     if (e.keyCode == 32) {
-        keyboard.SPACE = false;        
+        keyboard.SPACE = false;
     }
 
     if (e.keyCode == 68) {
-        keyboard.D = false;        
+        keyboard.D = false;
     }
 });
+
+
+function enterFullscreen() {
+    document.getElementById('canvas').style.width = "100%";
+    document.getElementById('canvas').style.height = "100vh";
+    document.getElementById('title').classList.add("d-none");
+    document.getElementById('instruction').classList.add("d-none");
+
+    let element = document.getElementById('fullscreen');
+
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+        element.msRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {  // iOS Safari
+        element.webkitRequestFullscreen();
+    }
+}
+
+
+function exitFullscreen() {
+    document.getElementById('canvas').style.width = "720px";
+    document.getElementById('canvas').style.height = "480px";
+    document.getElementById('title').classList.remove("d-none");
+    document.getElementById('instruction').classList.remove("d-none");
+
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+}
