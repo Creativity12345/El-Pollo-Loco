fd4bb711edae6d1f4f6e29741ac0e71610fecf53
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-01-11 13:43:33 +0100

    little changes

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -14,22 +14,21 @@ class DrawableObject {
         this.img.src = path;
     }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
     /**
     *       
     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
     */
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+        loadImages(arr) {
+            arr.forEach((path) => {
+                let img = new Image();
+                img.src = path;
+                this.imageCache[path] = img;
+            });
+        }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
 
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
@@ -41,3 +40,53 @@ class DrawableObject {
         }
     }
 }
+
+
+
+
+
+// class DrawableObject {
+//     x = 120;
+//     y = 0;
+//     height = 150;
+//     width = 100;
+//     img;
+//     imageCache = {};
+//     currentImg = 0;
+  
+//     loadImage(path) {
+//       this.img = new Image();
+//       this.img.src = path;
+//     }
+  
+//     loadImages(arr) {
+//       arr.forEach((path) => {
+//         let img = new Image();
+//         img.src = path;
+//         this.imageCache[path] = img;
+//       });
+//     }
+  
+//     draw(ctx) {
+//       ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+//     }
+  
+//     drawFrame(ctx) {
+//       if (
+//         this.canDrawFrame()
+//       ) {
+//         ctx.beginPath();
+//         ctx.lineWidth = "3";
+//         ctx.strokeStyle = "blue";
+//         ctx.rect(this.x, this.y, this.width, this.height);
+//         ctx.stroke();
+//       }
+//     }
+  
+//     canDrawFrame() {
+//      return this instanceof Character ||
+//       this instanceof Chicken ||
+//       this instanceof CollectableObject ||
+//       this instanceof Endboss
+//     }
+//   }
