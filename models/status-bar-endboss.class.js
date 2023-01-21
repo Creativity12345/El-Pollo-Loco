f9c53e0871229b6cec0ca6c6cb4de298a7e731f0
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-01-21 14:21:23 +0100

    add other endboss helath statusbar images

--- models/status-bar-endboss.class.js
+++ models/status-bar-endboss.class.js
@@ -1,12 +1,12 @@
 class StatusBarEndboss extends DrawableObject {
     percentage = 100;
     ENDBOSS_IMAGES = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png', //Bild 0
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png', //Bild 1
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png', //Bild 2
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png', //Bild 3
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png', //Bild 4
-        'img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png', //Bild 5
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/0.png', //Bild 0
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/20.png', //Bild 1
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/40.png', //Bild 2
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/60.png', //Bild 3
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/80.png', //Bild 4
+        'img/7_statusbars/1_statusbar/2_statusbar_health/orange/100.png', //Bild 5
     ];
 
 
