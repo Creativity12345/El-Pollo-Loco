6823190f185f726a0447e8b2757bacdec0177df2
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-01-21 14:45:07 +0100

    clear code

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,23 +1,21 @@
 class ThrowableObject extends MovableObject {
     images_bottleRotation = [
-      "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
-      "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
-      "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
-      "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+      'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+      'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+      'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+      'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
     ];
     images_bottleSplash = [
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
-      "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+      'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
     ];
   
     constructor(x, y) {
-      super().loadImage(
-        "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png"
-      );
+      super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
       this.loadImages(this.images_bottleRotation);
       this.loadImages(this.images_bottleSplash);
       this.x = x;
