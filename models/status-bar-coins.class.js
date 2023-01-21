3ac3a58def5bc71dbd1a23889d7a6b609c68f04c
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-01-21 13:14:34 +0100

    statusbar coins works

--- models/status-bar-coins.class.js
+++ models/status-bar-coins.class.js
@@ -27,18 +27,11 @@ class StatusBarCoins extends DrawableObject {
     }
 
     resolveImageIndex() {
-        if (this.percentage == 100) {
-            return 5;
-        } else if (this.percentage > 80) {
-            return 4;
-        } else if (this.percentage > 60) {
-            return 3;
-        } else if (this.percentage > 40) {
-            return 2;
-        } else if (this.percentage > 20) {
-            return 1;
-        } else {
-            return 0;
-        }
+        if (this.percentage >= 10) return 5;
+        else if (this.percentage > 8) return 4;
+        else if (this.percentage > 6) return 3;
+        else if (this.percentage > 4) return 2;
+        else if (this.percentage > 2) return 1;
+        else return 0;
     }
 }
