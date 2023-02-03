13570bcdbd74c192665472efd627b9c3d6e1c884
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-02-03 14:28:53 +0100

    character sounds mute works

--- js/game.js
+++ js/game.js
@@ -132,11 +132,21 @@ function removeClasses() {
 function mute() {
   if (!world.character.mute) {
     world.character.mute = true;
+    world.character.audio_background.volume = 0;
     world.character.audio_snore.volume = 0;
+    world.character.audio_walking.volume = 0;
+    world.character.audio_jump.volume = 0;
+    world.character.audio_lose.volume = 0;
+    // world.throwableobject.audio_throw.volume = 0;
   } 
   else {
     world.character.mute = false;
+    world.character.audio_background.volume = 1;
     world.character.audio_snore.volume = 1;
+    world.character.audio_walking.volume = 1;
+    world.character.audio_jump.volume = 1;
+    world.character.audio_lose.volume = 1;
+    // world.throwableobject.audio_throw.volume = 1;
   } 
 }
 
