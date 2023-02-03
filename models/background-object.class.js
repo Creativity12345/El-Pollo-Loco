9dabe6d2c907acdf702c4c3bec5660ebd24b7348
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-02-03 17:07:55 +0100

    add jsdocs for BackgroundObject

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -2,6 +2,12 @@ class BackgroundObject extends MovableObject{
     width = 720;
     height = 480;
 
+
+    /**
+    Create a BackgroundObject.
+    @param {string} imagePath - The path to the image.
+    @param {number} x - The x-coordinate of the object.
+    */
     constructor(imagePath, x){
         super().loadImage(imagePath);
         this.y = 480 - this.height;
