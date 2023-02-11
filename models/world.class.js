c50472fca8e1884b23372576a9748c5f4a2a8f48
Creativity12345 <dachrinnen.wohnwagen_0k@icloud.com>
2023-02-11 12:01:28 +0100

    better check endboss kill

--- models/world.class.js
+++ models/world.class.js
@@ -54,7 +54,7 @@ class World {
     }, 1000 / 60);
     setStoppableInterval(() => {
       this.checkEndbossKilled();
-    }, 200);
+    }, 500);
     setStoppableInterval(() => {
       this.checkSomeActivitiesOfGame();
     }, 50);
